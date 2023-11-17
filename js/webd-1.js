43fe8c52d95c84a40835a79af9ad5ba101b995e6
jieun419 <crg1050@naver.com>
2023-11-18 00:18:25 +0900

    feat: slider 작업

--- js/webd-1.js
+++ js/webd-1.js
@@ -9,3 +9,24 @@ navList.forEach((navItem) => {
     this.querySelector(".submenu").style.height = "0"
   })
 })
+
+/*slider*/
+const slider = document.querySelectorAll(".slider");
+
+slider.forEach(img => {
+  img.style.opacity = "0";
+  img.style.transition = "all 1s";
+});
+
+slider[0].style.opacity = "1";
+
+let currentIndex = 0;
+
+setInterval(() => {
+  let nextIndex = (currentIndex + 1) % slider.length;
+
+  slider[nextIndex].style.opacity = "1"
+  slider[currentIndex].style.opacity = "0"
+
+  currentIndex = nextIndex
+}, 3000)
